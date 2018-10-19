a8fd8911e83c7ef2821308934c9e89279218400e
mintdawn <38255303+mintdawn@users.noreply.github.com>
2018-10-19 00:43:54 -0500

    Update the Web Site

---
+++ dbconnect.php
@@ -0,0 +1,17 @@
+﻿<?php
+/*
+$dbServername = "localhost";
+$dbUsername = "unicorn_phpUser";
+$dbPassword = "iM6a=m6IEK[f";
+$dbName = "unicorn_learning";
+*/
+$dbServername = "localhost";
+$dbUsername = "noodleview";
+$dbPassword = "3iVANDaRHpwsizb";
+$dbName = "noodleworks";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+echo "connected";
+
+?>
+
