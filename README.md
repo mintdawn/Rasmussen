efe1f5aa333dc67c4ec8e8243001680a15e4f044
mintdawn <38255303+mintdawn@users.noreply.github.com>
2018-10-21 11:29:06 -0500

    Module 3 classwork uploaded.

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ This is an imaginary site done for class. Eventually it will connect to a databa
 10/14/2018
 
 Responsiveness added to the navbar and logo moved to the navbar. Jumbotron added at the top with just the company name in it. The make the jumbotron and the navbar go to the edge of the main wrapper div I had to put them in a row class div and add 100% width styling to the css file. I learned a ton about bootstrap and learned that there is a whole responsive system for padding and margins that can be used with a lot of the elements. The entire site should be working okay on a mobile device now.
+
+10/21/2018
+
+This week I got the database connected to Azure and formatted the noodles.php page to display the data received from Azure. The file noodles.html had to be changed to noodles.php to work correctly. I found that bootstrap classes worked within php and that made things a lot easier. I was able to display the data with very little problem. I also added a little javascript one page2.html to create a page that generates random facts about Asian noodles. I removed several .php files from the repository that I no longer need for this project. There is a file called main.html in the repository. That file is the index page for the root of my Azure school project. It is unrelated to the Noodle Company site. Next week we will be working with sitemaps. 
--- noodle.php
+++ noodle.php
@@ -1,9 +1,10 @@
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Lucky Cat Noodle Company</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lucky Cat Noodle Company</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<link rel="stylesheet" href="styles/index.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Courgette">
@@ -13,7 +14,8 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
-  <body>
+
+ <body>
 <!-- main container for site -->
   <div class="container-fluid text-center bg-warning" id="wrapper">
 
@@ -23,7 +25,7 @@
 			<h1><a class="text-warning" href="index.html" style="text-decoration: none;">Lucky Cat Noodle Company</a></h1>
 		</div>
 	</div>
-    
+
 <!-- top navbar -->
 	<div class="row">
 		<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top" id="topbar">
@@ -36,35 +38,65 @@
 	
 		<div class="navbar-nav">
 			<a class="nav-item nav-link" href="index.html">Home</a>
-			<a class="nav-item nav-link" href="noodle.php">Noodle Database</a>
+			<a class="nav-item nav-link active" href="noodle.php">Noodle Database</a>
 			<a class="nav-item nav-link" href="page2.html">Noodle Facts</a>
-			<a class="nav-item nav-link active" href="page3.html">How to Eat</a>
+			<a class="nav-item nav-link" href="page3.html">How to Eat</a>
 		</div>
 		</div>
 		</nav>
 	</div>
-           
-    <div class="container-fluid">
-     <h3>How to Eat Noodles</h3>
-       </div>
-<!-- youtube video -->
-	<div class="container-fluid">
-		<div class="embed-responsive embed-responsive-21by9">
-		<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/9MrL90zksKc" allowfullscreen></iframe>
-		</div>
+
+   <div class="container-fluid">
+     <h3>Ramen Noodle Database</h3>
     </div>
-	
+
+<div class="container datainfo bg-light">
+<?php
+//Allow Headers 
+header('Access-Control-Allow-Origin: *');
+//$servername = "localhost:3306"; 
+$servername = "localhost:3306";
+$username = "noodleview"; 
+$password = "test"; 
+$dbname = "noodleworks";
+// Create connection 
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection 
+ if ($conn->connect_error) 
+     echo "Error: Unexpected connection error. Please retry the operation."; 
+ else 
+  { 
+     $result = $conn->query("SELECT * FROM noodles");
+     if (($result != 0) && ($result->num_rows > 0)) 
+       { 
+          while($row = $result->fetch_assoc()) {
+          $id = $row['id']; 
+          $name = $row['name']; 
+          $type = $row['type']; 
+          $desc = $row['description'];
+          echo '<div class="card result text-left m-2 p-2">'; 
+          echo '<h4 class="text-capitalize">'.$id.'.     '.$name.'</h4>'; 
+          echo '<p>Type:  '.$type.'</p>'; 
+          echo '<p>'.$desc.'</p></div>';
+		  }		  
+       } 
+     $conn->close(); 
+}
+?>
+</div>
+
 <!-- footer -->
 	<div class="row">
 		<div class="col-sm-12 col-lg-12 bg-dark footer">
-			<p class="text-light float-left">page3.html</p>
+			<p class="text-light float-left">noodle.php</p>
 			<p class="text-light float-right">Dawn Summerall - 2018</p>
 		</div>
     </div>
-	
+
   </div>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  </body>
-  </html>
+  <script src="scripts/index.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
